32e23cf6da6d21b7d22af69b6c9389f0c36617d5
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2023-02-12 08:53:08 +0100

    CDROM.INI
    If not present use getcdw
    if present check if ./ present and remove it for MorphOS

--- ___3e3cch.c
+++ ___3e3cch.c
@@ -1,6 +1,6 @@
 #include "drally.h"
-
-	extern char ___1a0d60h[0xc8];
+#include <unistd.h>
+extern char ___1a0d60h[0xc8];
 
 void dRally_System_clean(void);
 
@@ -9,14 +9,26 @@ void ___3e3cch(void){
     FILE *  fd;
 
     if(!(fd = strupr_fopen("CDROM.INI", "rb"))){
+		
+		getcwd(___1a0d60h, sizeof(___1a0d60h));;
+		strcat(___1a0d60h, "/");
+		
+        //printf("Error reading CDROM.INI file!\n");
+        //dRally_System_clean();
+        //exit(0x70);
+		
+    } else {
 
-        printf("Error reading CDROM.INI file!\n");
-        dRally_System_clean();
-        exit(0x70);
-    }
+		fscanf(fd, "%s", ___1a0d60h);
+		fclose(fd);
 
-    fscanf(fd, "%s", ___1a0d60h);
-    fclose(fd);
-
-    if(___1a0d60h[strlen(___1a0d60h)-1] != '/') strcat(___1a0d60h, "/");
+		#ifdef __MORPHOS__
+		if (___1a0d60h[0] == '.' && ___1a0d60h[1] == '/') {
+			memmove(___1a0d60h, ___1a0d60h + 2, strlen(___1a0d60h));
+		}
+		#endif
+		
+		if(___1a0d60h[strlen(___1a0d60h)-1] != '/') strcat(___1a0d60h, "/");
+		
+	}
 }
