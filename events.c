5923db863d8f8fb8bde71900af8ce50e510ecb51
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2023-03-03 20:00:29 +0100

    MorphOS: use switch instead of if conditions

--- events.c
+++ events.c
@@ -38,9 +38,11 @@ void IO_Loop(void){
 
     while(SDL_PollEvent(&e)){
 
-        if(e.type == SDL_KEYDOWN){
-
-			if (e.key.keysym.mod & KMOD_ALT) {
+		switch (e.type) 
+		{
+			
+			case SDL_KEYDOWN:
+				if (e.key.keysym.mod & KMOD_ALT) {
 				if (e.key.keysym.sym == SDLK_RETURN) {
 					SetFullscreen();
 				} else if (e.key.keysym.sym == SDLK_F3 ) {
@@ -51,54 +53,54 @@ void IO_Loop(void){
 					bIntegerScale = !bIntegerScale;
 					SDL_RenderSetIntegerScale(GX.Renderer, (bIntegerScale?SDL_TRUE:SDL_FALSE));			
 				}
-			} else {			
-            	dRally_Keyboard_make(e.key.keysym.scancode);
-			}
-        }
-        else if(e.type == SDL_KEYUP){
-           
-            dRally_Keyboard_break(e.key.keysym.scancode);
-        }
-		else if (e.type == SDL_JOYBUTTONDOWN || e.type == SDL_JOYBUTTONUP)  {
-			SDL_Scancode bu = 0;
-			switch (e.jbutton.button) {
-				case 2: //START
-				case 7: //A
-					bu = SDL_SCANCODE_RETURN;
-					break;
-				case 4: // SELECT
-				case 3: //BAK
-					bu = SDL_SCANCODE_ESCAPE;
-					break;
-				case 8: // B
-					bu = SDL_SCANCODE_DELETE;
-					break;
-				case 6: // Y
-					bu = SDL_SCANCODE_SPACE;
-					break;
-				case 5: // X
-					bu = SDL_SCANCODE_LALT;
-					break;
-				case 0:
-					bu = SDL_SCANCODE_LCTRL;
-					break;
-				case 1:
-					bu = SDL_SCANCODE_LSHIFT;
-					break;
-				default:
-					break;
-			}
-			if (bu > 0) {
-				if (e.type == SDL_JOYBUTTONDOWN)
-					dRally_Keyboard_make(bu);	
-				else
-					dRally_Keyboard_break(bu);
-			}
-		}
-		else if (e.type == SDL_JOYHATMOTION)
-		{
-			switch (e.jhat.value)
-			{
+				} else {			
+					dRally_Keyboard_make(e.key.keysym.scancode);
+				}
+				
+				break;
+			case SDL_KEYUP:
+				dRally_Keyboard_break(e.key.keysym.scancode);
+				break;
+			case SDL_JOYBUTTONDOWN:
+			case SDL_JOYBUTTONUP:
+				SDL_Scancode bu = 0;
+				switch (e.jbutton.button) {
+					case 2: //START
+					case 7: //A
+						bu = SDL_SCANCODE_RETURN;
+						break;
+					case 4: // SELECT
+					case 3: //BAK
+						bu = SDL_SCANCODE_ESCAPE;
+						break;
+					case 8: // B
+						bu = SDL_SCANCODE_DELETE;
+						break;
+					case 6: // Y
+						bu = SDL_SCANCODE_SPACE;
+						break;
+					case 5: // X
+						bu = SDL_SCANCODE_LALT;
+						break;
+					case 0:
+						bu = SDL_SCANCODE_LCTRL;
+						break;
+					case 1:
+						bu = SDL_SCANCODE_LSHIFT;
+						break;
+					default:
+						break;
+				}
+				if (bu > 0) {
+					if (e.type == SDL_JOYBUTTONDOWN)
+						dRally_Keyboard_make(bu);	
+					else
+						dRally_Keyboard_break(bu);
+				}
+				break;
+			case SDL_JOYHATMOTION:
+				switch (e.jhat.value)
+				{
 				case SDL_HAT_UP:
 					dRally_Keyboard_make(SDL_SCANCODE_UP);
 					break;
@@ -133,50 +135,51 @@ void IO_Loop(void){
 					dRally_Keyboard_break(SDL_SCANCODE_LEFT);
                     dRally_Keyboard_break(SDL_SCANCODE_RIGHT);
 					break;
-			}
-		}
-		else if (e.type == SDL_JOYAXISMOTION) {
-            switch (e.jaxis.axis) {
-            case 0:
-                if (e.jaxis.value > 20000) {//RIGHT
-                    dRally_Keyboard_make(SDL_SCANCODE_RIGHT);
-                }
-                else if (e.jaxis.value < -20000) {//LEFT
-                    dRally_Keyboard_make(SDL_SCANCODE_LEFT);
-                }
-                else {
-                    dRally_Keyboard_break(SDL_SCANCODE_RIGHT);
-                    dRally_Keyboard_break(SDL_SCANCODE_LEFT);
-                }
-                break;
-            case 1:
-                if (e.jaxis.value > 20000) {//DOWN
-                    dRally_Keyboard_make(SDL_SCANCODE_DOWN);
-                }
-                else if (e.jaxis.value < -20000) {//UP
-                    dRally_Keyboard_make(SDL_SCANCODE_UP);
-                } else {
-					dRally_Keyboard_break(SDL_SCANCODE_DOWN);
-                    dRally_Keyboard_break(SDL_SCANCODE_UP);
 				}
-                break;
-            }
-
-
-        }
-        else if(e.type == SDL_QUIT){
+				break;
+			case SDL_JOYAXISMOTION:
+				switch (e.jaxis.axis) {
+					case 0:
+						if (e.jaxis.value > 20000) {//RIGHT
+							dRally_Keyboard_make(SDL_SCANCODE_RIGHT);
+						}
+						else if (e.jaxis.value < -20000) {//LEFT
+							dRally_Keyboard_make(SDL_SCANCODE_LEFT);
+						}
+						else {
+							dRally_Keyboard_break(SDL_SCANCODE_RIGHT);
+							dRally_Keyboard_break(SDL_SCANCODE_LEFT);
+						}
+						break;
+					case 1:
+						if (e.jaxis.value > 20000) {//DOWN
+							dRally_Keyboard_make(SDL_SCANCODE_DOWN);
+						}
+						else if (e.jaxis.value < -20000) {//UP
+							dRally_Keyboard_make(SDL_SCANCODE_UP);
+						} else {
+							dRally_Keyboard_break(SDL_SCANCODE_DOWN);
+							dRally_Keyboard_break(SDL_SCANCODE_UP);
+						}
+						break;
+				}
+				break;
+			case SDL_QUIT:
 #if _DEBUG
-            printf("DEATH RALLY Exit: SDL QUIT pressed!\n");
+            	printf("DEATH RALLY Exit: SDL QUIT pressed!\n");
 #endif
 #if defined(DR_MULTIPLAYER)
-			if(___19bd60h != 0) ___623d4h();
+				if(___19bd60h != 0) ___623d4h();
 #endif
+				dRally_Sound_quit();
+				dRally_Display_clean();
+				dRally_System_clean();
+				exit(0x70);
+				break;
+			default:
+				break;	
+			
+		}
 
-			dRally_Sound_quit();
-			dRally_Display_clean();
-			dRally_System_clean();
-
-			exit(0x70);
-        }
     }
 }
